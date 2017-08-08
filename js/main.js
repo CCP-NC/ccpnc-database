0f6ed0974fcd0c62f3061c8654e9b9b203c9bc18
Simone Sturniolo <wml08325@scpc044.esc.rl.ac.uk>
2017-08-08 11:40:31 +0100

    Fixed the angular loading issue with manual bootstrapping

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 (function() {
 
+  console.log('Creating Angular app...');
   var ccpncApp = angular.module('ccpncDatabaseApp', []);
 
   // Add controllers
+  console.log('Loading controllers...');
   addLoginController(ccpncApp);
+
+  // Manual bootstrap (automatic one creates problem with order in which scripts are loaded)
+  console.log('Bootstrapping app...');
+  angular.element(function() {
+      angular.bootstrap(document, ['ccpncDatabaseApp']);
+  });
+  console.log('Success');
 
 })();
