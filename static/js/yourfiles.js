3ea96e14bffb48243afff3b38252d96b856bdd3f
stur86 <simonesturniolo@gmail.com>
2018-01-12 15:48:50 +0000

    Fixed issues with yourfiles.js, added error message for failed connection

--- static/js/yourfiles.js
+++ static/js/yourfiles.js
@@ -3,29 +3,41 @@ function addFileListerController(ngApp) {
 
     ngApp.controller('FileListerController', function($scope, loginStatus) {
 
+        $scope.message = '';
+
         // This code gets executed every time the tab is opened, and refreshes
         // the contents
 
         details = loginStatus.get_details();
 
         // Perform a search for files with same ORCID
-        query =  {
-            url: '/search', 
-            type: 'POST', 
-            crossDomain: true, 
-            contentType: 'application/json', 
-            data: JSON.stringify({'search_spec': [{
-                'type': 'orcid',
-                'orcid': details.orcid,
-                },]
-            })
-        }
+        query = {
+            url: '/search',
+            type: 'POST',
+            crossDomain: true,
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'search_spec': [{
+                    'type': 'orcid',
+                    'args': {
+                        'orcid': details.orcid,
+                    },
+                }, ]
+            }),
+            success: function(d) {
+                $scope.search_results = parseSearchResults(d);
+                $scope.message = '';
+                $scope.$apply();
+            },
+            error: function(d) {
+                console.log(d);
+                $scope.message = 'ERROR: connection failed - ' + d.statusText;
+                $scope.$apply();
+            },
+            timeout: 1000
+        };
 
-        $.ajax(query).done(function(d) {
-            $scope.search_results = parseSearchResults(d);
-            //console.log($scope.search_results);
-            $scope.$apply();
-        });
+        $.ajax(query);
 
     });
 }
