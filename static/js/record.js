0cdb80253d01e6328b8d08a8206e50b34dea63f2
stur86 <simonesturniolo@gmail.com>
2020-05-15 16:46:36 +0100

    Added calculation details

--- static/js/record.js
+++ static/js/record.js
@@ -15,9 +15,6 @@ function addRecordDirective(ngApp) {
                 scope._edit_popup = {};
                 scope.is_page = attr['makePage'] != null;
 
-                // It's important to use "var" here and keep the scope local
-                // or there's some reference shenanigans...
-                var index_id = scope.databaseRecord.index_id;
 
                 scope.isown = function() {
                     if (!loginStatus.get_login_status()) {
@@ -28,68 +25,71 @@ function addRecordDirective(ngApp) {
                     }
                 }
 
-                scope.edit = function() {
-                    this._edit_popup = new editPopup(this, 
-                                                    this.databaseRecord.chemname,
-                                                    this.databaseRecord.version_history[this._selected_index],
-                                                    function(scope) {
+                // It's important to use "var" here and keep the scope local
+                // or there's some reference shenanigans...
+                // var index_id = scope.databaseRecord.index_id;
+                // scope.edit = function() {
+                //     this._edit_popup = new editPopup(this, 
+                //                                     this.databaseRecord.chemname,
+                //                                     this.databaseRecord.version_history[this._selected_index],
+                //                                     function(scope) {
                         
-                        // Please note: "this" here is the popup, NOT the record!
-                        // Refer to edit.js to actually see the object
-                        // This method is encapsulated anonymously here for security
+                //         // Please note: "this" here is the popup, NOT the record!
+                //         // Refer to edit.js to actually see the object
+                //         // This method is encapsulated anonymously here for security
 
-                        request_data = $.extend({
-                            index_id: index_id, 
-                        }, this._table.get_props());                        
-                        if (this.magres_file_name != '')
-                            request_data['magres'] = this.magres_file;
+                //         request_data = $.extend({
+                //             index_id: index_id, 
+                //         }, this._table.get_props());                        
+                //         if (this.magres_file_name != '')
+                //             request_data['magres'] = this.magres_file;
 
-                        var request_data = {
-                            index_id: index_id, 
-                        }
-                        popup = this;
+                //         var request_data = {
+                //             index_id: index_id, 
+                //         }
+                //         popup = this;
 
-                        loginStatus.verify_token(function() {
-                            // Package all the data
-                            details = loginStatus.get_details()
-                            request_data.access_token = details['access_token'];
-                            request_data.orcid = details['orcid'];
+                //         loginStatus.verify_token(function() {
+                //             // Package all the data
+                //             details = loginStatus.get_details()
+                //             request_data.access_token = details['access_token'];
+                //             request_data.orcid = details['orcid'];
 
-                            // Send an Ajax request
-                            popup.uploading_now = true;
-                            scope.$apply();
+                //             // Send an Ajax request
+                //             popup.uploading_now = true;
+                //             scope.$apply();
 
-                            $('#edit-popup-form').ajaxSubmit({
-                                data: request_data,
-                                success: function(r) {
-                                    // Did anything go wrong?
-                                    if (r != 'Success') {
-                                        scope.status = 'ERROR: ' + r;
-                                        scope.status_err = true;
-                                    } else {
-                                        scope.$parent.$parent.refresh();
-                                        scope.cancel()
-                                    }
+                //             $('#edit-popup-form').ajaxSubmit({
+                //                 data: request_data,
+                //                 success: function(r) {
+                //                     // Did anything go wrong?
+                //                     if (r != 'Success') {
+                //                         scope.status = 'ERROR: ' + r;
+                //                         scope.status_err = true;
+                //                     } else {
+                //                         scope.$parent.$parent.refresh();
+                //                         scope.cancel()
+                //                     }
 
-                                    popup.uploading_now = false;
-                                    scope.$apply();
+                //                     popup.uploading_now = false;
+                //                     scope.$apply();
 
-                                },
-                                error: function(e) {
-                                    popup.status = 'ERROR: ' + e.responseText;
-                                    popup.status_err = true;
-                                    popup.uploading_now = false;
-                                    scope.$apply();
-                                }
-                            });                            
+                //                 },
+                //                 error: function(e) {
+                //                     popup.status = 'ERROR: ' + e.responseText;
+                //                     popup.status_err = true;
+                //                     popup.uploading_now = false;
+                //                     scope.$apply();
+                //                 }
+                //             });                            
 
-                        }, function() {
-                            popup.status = 'Could not authenticate ORCID details; please log in'
-                            popup.status_err = true;
-                            console.log(popup.status);
-                        });                        
-                    });
-                };
+                //         }, function() {
+                //             popup.status = 'Could not authenticate ORCID details; please log in'
+                //             popup.status_err = true;
+                //             console.log(popup.status);
+                //         });                        
+                //     });
+                // };
 
                 scope.filename = function() {
                     return this.databaseRecord.chemname + '_v' + (parseInt(this._selected_index)+1) + '.magres';
@@ -104,6 +104,13 @@ function addRecordDirective(ngApp) {
                     date = date[2] + '-' + date[1] + '-' + date[0];
 
                     return date + ' ' + time;
+                }
+
+                // Magres calc blocks
+                scope.mcalc_blocks = [];
+
+                for (var i = 0; i < scope.databaseRecord.version_history.length; ++i) {
+                    scope.mcalc_blocks.push(JSON.parse(scope.databaseRecord.version_history[i].magres_calc));
                 }
             }
         };
