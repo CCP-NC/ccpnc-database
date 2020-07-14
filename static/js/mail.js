156b48a4c2764779f197716fa861c89918ed21d6
stur86 <simonesturniolo@gmail.com>
2020-07-14 11:03:59 +0100

    Added status message to mail form

--- static/js/mail.js
+++ static/js/mail.js
@@ -5,13 +5,21 @@ function addMailController(ngApp) {
             email: ''
         };
 
+        $scope.status = '';
+        $scope.status_err = false;
+
         $scope.sendmail = function() {
             $('#contact-form').ajaxSubmit({
                 success: function(msg, status) {
-
+                    $scope.status = 'Message sent successfully';
+                    $scope.status_err = false;
+                    $scope.$apply();
+                    $('#contact-form')[0].reset();
                 },
                 error: function(err) {
-                    
+                    $scope.status = err.responseText;
+                    $scope.status_err = true;
+                    $scope.$apply();
                 }
             });
         };
