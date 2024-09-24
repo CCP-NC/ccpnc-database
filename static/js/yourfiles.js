39a35f6ba302e6f7573130f751b1fe28164ac3ed
Sathya Sai Seetharaman <s3.at.work@gmail.com>
2024-09-24 11:09:10 +0000

    Boolean choice added to data payload

--- static/js/yourfiles.js
+++ static/js/yourfiles.js
@@ -25,6 +25,9 @@ function addFileListerController(ngApp) {
                     'args': {
                         'orcid': details.orcid,
                     },
+                    'boolean': false
+                    //'boolean' variable tracks user's choice of AND/OR using the toggle switch
+                    // for displaying user uploaded files, default value of false is set constantly
                 }, ]
             }),
             success: function(d, statusText, xhr) {
